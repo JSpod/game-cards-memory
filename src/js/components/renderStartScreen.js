088cf290805097f4b2a1c96702e9ebcdfefbca7d
Dmitriy Stor <stordbox@gmail.com>
2023-02-21 23:49:02 +0100

    Add | js | components | Start screen

---
+++ src/js/components/renderStartScreen.js
@@ -0,0 +1,8 @@
+function renderStartScreen(cb) {
+  const base = document.querySelector('.base') // Get the base element
+  base.textContent = '' // clear the base element
+  app.renderBlock('start-block', base) // render the start block in the base element
+  cb()
+}
+
+export default renderStartScreen
