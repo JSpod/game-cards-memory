f04678cf8ccf75356ec230414c9c5a5d7ec51cfc
Dmitriy Stor <stordbox@gmail.com>
2023-03-06 23:03:29 +0100

    Add | js | components | Game screen

---
+++ src/js/components/renderGameScreen.js
@@ -0,0 +1,8 @@
+function renderGameScreen(cb) {
+  const base = document.querySelector('.base') // Get the base element
+  base.textContent = '' // Clear the base element
+  app.renderBlock('game-block', base) // Render the block in the base element
+  if (cb) cb() // Call the callback function if it exists
+}
+
+export default renderGameScreen
